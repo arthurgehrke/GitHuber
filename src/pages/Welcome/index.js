498795c1939beb6a9f6a172282980a3ac77ac8b7
Arthur Gehrke <arthurgehrke07@gmail.com>
2020-01-07 16:37:48 -0300

    estilizando a pagina inicial

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar
+} from "react-native";
 
 import styles from "./styles";
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Teste</Text>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para você continuar precisamos que você informe seu usuário no GitHub
+    </Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none" //Para não colocar letra maiúscula no começo automaticamente
+        autoCorrect={false} //Para não corrigir automaticamente
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent" //Para colocar a linha em baixo da letra no android como transp.
+      ></TextInput>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
