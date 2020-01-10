340e1f5ff7d0adc73d8777567f1aba3c8be54767
Arthur Gehrke <arthurgehrke07@gmail.com>
2020-01-10 15:25:43 -0300

    configurando a rota inicial com base no usuário logado

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
 
-import Routes from "./src/routes";
+import createNavigator from "./src/routes";
 
-export default function App() {
-  return <Routes />;
+export default class App extends Component {
+  state = {
+    userChecked: false, //Verifica-se se a informação do login estava no async storage, vira true alguma hora (em que se verifica)
+    userLogged: false // Verficia se a EXISTE está no async storage, vira true apenas se a informação estiver no asyncStorage
+  };
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem("@Githuber:username");
+
+    this.setState({
+      userChecked: true, //Verificou-se a existencia ou não do user,
+      userLogged: !!username //Verifica se existe o username, com o !! inverte-se o valor booleano, passando assim para true, se não existir, continua falso
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null; //Se userchecked estiver falso, retorna-se null, se estiver true, renderiza-se as rotas
+
+    const Routes = createNavigator(userLogged);
+    return <Routes />;
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,19 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import Welcome from "./pages/Welcome";
 import Repositories from "./pages/Repositories";
 
-const Routes = createAppContainer(
-  // o appContainer deve envolver o switchNavigator, que conterá os componentes de rotas
-  createSwitchNavigator({
-    Welcome, // há varios navigators
-    Repositories // dentro, vão os componentes de paginas (rotas) que serão navegadas
-  })
-);
+//Transformou-se em uma função, para se receber o userlogged e se definir qual sera a rota inicial
+const Routes = (userLogged = false) =>
+  createAppContainer(
+    // o appContainer deve envolver o switchNavigator, que conterá os componentes de rotas
+    createSwitchNavigator(
+      {
+        Welcome, // há varios navigators
+        Repositories // dentro, vão os componentes de paginas (rotas) que serão navegadas
+      },
+      {
+        initialRouteName: userLogged ? "Repositories" : "Welcome" //Se ele já tiver o usuário armazenado no local storage, pular a pagina de login
+      }
+    )
+  );
 
 export default Routes;
