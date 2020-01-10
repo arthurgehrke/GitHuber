7622e692f483426d37a0b2c2abc21c1a6cb0c469
Arthur Gehrke <arthurgehrke07@gmail.com>
2020-01-10 16:07:10 -0300

    adicionando header na pagína principal

---
+++ src/components/Header/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { colors, metrics } from "../../styles";
+import { getStatusBarHeight } from "react-native-status-bar-height"; //Função para pegar a altura da status bar do cel
+
+const styles = StyleSheet.create({
+  container: {
+    height: 54 + getStatusBarHeight(),
+    paddingTop: getStatusBarHeight(),
+    borderBottomWidth: 1,
+    borderBottomColor: colors.light,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: metrics.basePadding
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: colors.darker
+  }
+});
+
+export default styles;
+
+//yarn add react-native-status-bar-height para alterar o tamanho da barra de status, cada versão de celular tem um tamanho diferente
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import { View } from "react-native";
 
-const Repositories = () => <View />;
+import Header from "../../components/Header";
+const Repositories = () => (
+  <View>
+    <Header title="Repositórios" />
+  </View>
+);
 
 export default Repositories;
