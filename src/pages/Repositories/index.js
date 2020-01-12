57acaf3900c259c963dd06683b686365df573da8
Arthur Gehrke <arthurgehrke07@gmail.com>
2020-01-12 01:45:55 -0300

    adicionando os repositórios

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,22 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import api from "../../services/api";
 
-import { View } from "react-native";
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList
+} from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import Header from "../../components/Header";
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
+import RepositoryItem from "./RepositoryItem";
+import styles from "./styles";
+
+const TabIcon = ({ tintColor }) => (
+  <Icon name="list-alt" size={20} color={tintColor} />
 );
 
-//Propriedade estática que o rn busca algumas configurações específicas
-Repositories.navigationOptions = {
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="list-alt" size={20} color={tintColor} />
-  )
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired
 };
 
-export default Repositories;
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon
+  };
+
+  state = {
+    data: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem("@Githuber:username");
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => {
+    const { data } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)} //no github, cada repositório tem um id, então a key será por ele
+        renderItem={this.renderListItem}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Repositórios" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
