b963e08ad89a384294d5d7de7654440e68f2d9d3
Arthur Gehrke <arthurgehrke07@gmail.com>
2020-01-12 02:30:44 -0300

    reparos finais/extraçao de organizações

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,22 +1,77 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import api from "../../services/api";
 
-import { View } from "react-native";
+import { View, AsyncStorage, FlatList, ActivityIndicator } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import Header from "../../components/Header";
+import OrganizationItem from "./OrganizationItem";
+import styles from "./styles";
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
+const TabIcon = ({ tintColor }) => (
+  <Icon name="building" size={20} color={tintColor} />
 );
 
-//Propriedade estática que o rn busca algumas configurações específicas
-Organizations.navigationOptions = {
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="building" size={20} color={tintColor} />
-  )
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired
 };
 
-export default Organizations;
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon
+  };
+
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem("@Githuber:username");
+    const { data } = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)} //no github, cada repositório tem um id, então a key será por ele
+        renderItem={this.renderListItem}
+        onRefresh={this.loadOrganizations}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
